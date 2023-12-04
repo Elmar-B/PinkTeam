483a68118c170ce254ea12c5d635c3e75c23e258
Elmar-B <elmarb21@ru.is>
2023-12-04 15:18:42 +0000

    Sword and tilemap boundry

---
+++ RobberJotunn/Assets/Scripts/SwordSwipeAttackController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSwipeAttackPhysics : MonoBehaviour
+{
+    public float startingRotationSpeed;
+    private float rotationSpeed;
+    public bool swing;
+    public bool rightSwing;
+
+    void Start()
+    {
+        if (rightSwing)
+            transform.rotation = Quaternion.Euler(Vector3.forward * -120);
+        else
+        {
+            transform.rotation = Quaternion.Euler(Vector3.forward * 120);
+            startingRotationSpeed = -startingRotationSpeed;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rotationSpeed = startingRotationSpeed * (1f - Mathf.Abs(transform.rotation.z));
+        Debug.Log(transform.rotation.z);
+
+        if (transform.rotation.z > 0.9 || transform.rotation.z < -0.9)
+            Destroy(gameObject);
+
+        if (swing)
+            transform.Rotate(0, 0, rotationSpeed * Time.fixedDeltaTime);
+    }
+}

bee8c1cff7fe79554c4503a4dacd89995d188207
bjarnibaerings <bjarnibh@outlook.com>
2023-12-11 12:37:38 +0000

    Made spear

--- RobberJotunn/Assets/Scripts/JotunSpear.cs
+++ RobberJotunn/Assets/Scripts/JotunSpear.cs
@@ -5,21 +5,39 @@ using UnityEngine;
 
 public class JotunSpear : MonoBehaviour
 {
-    public float startingStapSpeed;
-    private float stapSpeed;
-    public bool stap;
+    private Rigidbody2D myRigidBody;
+    public float velocity;
+    public float timeToDestroy;
+    private bool isMoving = false;
+    private GameObject playerObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        playerObj = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (stap)
+        if(!isMoving){
+            Vector3 direction = new(0,-1,0);
+            myRigidBody.velocity = velocity * direction;
+            isMoving = true;
+        }
+
+        timeToDestroy -= Time.deltaTime;
+        if(timeToDestroy <= 0)
         {
-            transform.position += new Vector3(0,2* Time.deltaTime,0);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
         }
     }
 }
